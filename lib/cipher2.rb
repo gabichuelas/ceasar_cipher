9200046c1fd5dd85ea01ccd8e9951604a80802be
Gaby Mendez <gmendez90@gmail.com>
2020-06-05 22:53:44 -0400

    Create two other cipher versions

--- lib/cipher2.rb
+++ lib/cipher2.rb
@@ -1,7 +1,5 @@
-# this solution uses zip, then nested iteration to
-# find matching characters in alphabet key
-
-class Cipher
+# this solution uses zip, then to_h to create alphabet key
+class CipherTwo
 
   ALPHA = ("a".."z").to_a
 
@@ -12,12 +10,20 @@ class Cipher
       if ALPHA.include?(char) == false
         result << char
       else
-        make_cipher(shift).each do |pair|
-          result << pair[1] if pair[0] == char
-        end
+        result << make_cipher(shift)[char]
       end
     end
     result.join.upcase
+  end
+
+  def make_cipher(shift)
+    cipher = ALPHA.rotate(-shift)
+    ALPHA.zip(cipher).to_h
+  end
+
+  def solve_cipher(shift)
+    solved = ALPHA.rotate(shift)
+    ALPHA.zip(solved).to_h
   end
 
   def decode(message, shift)
@@ -27,24 +33,14 @@ class Cipher
       if ALPHA.include?(char) == false
         result << char
       else
-        solve_cipher(shift).each do |pair|
-          result << pair[1] if pair[0] == char
-        end
+        result << solve_cipher(shift)[char]
       end
     end
     original = result.join.split(" ")
     original.map { |word| word.capitalize }.join(" ")
   end
 
-  def make_cipher(shift)
-    cipher = ALPHA.rotate(-shift)
-    ALPHA.zip(cipher)
-  end
 
-  def solve_cipher(shift)
-    solved = ALPHA.rotate(shift)
-    ALPHA.zip(solved)
-  end
 
 end
 
--- test/cipher_test.rb
+++ test/cipher_test.rb
@@ -1,10 +1,12 @@
 require "minitest/autorun"
 require "minitest/pride"
 require "./lib/cipher"
+require "./lib/cipher2"
 
 class CipherTest < Minitest::Test
   def setup
     @cipher = Cipher.new
+    @cipher2 = CipherTwo.new
   end
 
   def test_it_exists
@@ -13,10 +15,16 @@ class CipherTest < Minitest::Test
 
   def test_it_can_encode
     assert_equal "CZGGJ RJMGY", @cipher.encode("Hello World", 5)
+    assert_equal "CZGGJ RJMGY", @cipher2.encode("Hello World", 5)
+
   end
 
   def test_it_can_decode
     encrypted = @cipher.encode("Hello World", 5)
+    encrypted2 = @cipher2.encode("Hello World", 5)
+
     assert_equal "Hello World", @cipher.decode(encrypted, 5)
+    assert_equal "Hello World", @cipher2.decode(encrypted2, 5)
+
   end
 end
